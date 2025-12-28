1a9fec49362fbbb2c8768477b42e27eb6cdd5c91
sudarshankumar07 <sudarshankumar84002@gmail.com>
2025-12-28 13:00:06 +0530

    Update register.js

--- static/js/register.js
+++ static/js/register.js
@@ -1,3 +1,18 @@
+login = document.querySelector(".login-btn")
+login.addEventListener("click",()=>callAPI("/api/login"))
+let msg = document.querySelector(".none")
+let msg1 = document.querySelector("#msg1")
+
+function callAPI(url){
+    fetch(url,{method:"POST"})
+    .then(res => res.json())
+    .then(data=>{
+        if(data.success){
+            window.location.href = data.redirect;
+        }
+    })
+    .catch(err=>console.error(err))
+}
 let msg = document.querySelector(".none")
 let msg1 = document.querySelector("#msg1")
 
@@ -37,5 +52,6 @@ signup.addEventListener("click",async()=>{
 
 
 })
+
 
 
